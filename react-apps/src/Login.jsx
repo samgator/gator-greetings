1d4f15e2d8a67c755b32f208ce55e62f438b099b
kprhoads <krhoads325@gmail.com>
2025-02-27 20:27:09 -0500

    Added input fields for email and password

--- react-apps/src/Login.jsx
+++ react-apps/src/Login.jsx
@@ -1,10 +1,12 @@
 function Login() {
     return(
-        <div className="login-container">
+        <div className="container">
             <div className="login">
                 <img src="https://placehold.co/200"></img> {/* Enter logo here later */}
                 <h1>Welcome to GatorGreetings</h1>
-                <h2>Please log in below</h2>
+                <h2>Please log in or sign up below</h2>
+                <div><input type="text" placeholder="Email address"/></div>
+                <div><input type="text" placeholder="Password"/></div>
             </div>
         </div>
     );
