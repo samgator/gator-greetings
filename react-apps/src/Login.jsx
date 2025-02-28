808624e405a7602484ddebf1daf3205314d0e4af
kprhoads <krhoads325@gmail.com>
2025-02-27 20:01:43 -0500

    Created base skeleton for login page

--- react-apps/src/Login.jsx
+++ react-apps/src/Login.jsx
@@ -1,8 +1,12 @@
-import Header from './Header.jsx'
-
 function Login() {
     return(
-        <Header/>
+        <div className="login-container">
+            <div className="login">
+                <img src="https://placehold.co/200"></img> {/* Enter logo here later */}
+                <h1>Welcome to GatorGreetings</h1>
+                <h2>Please log in below</h2>
+            </div>
+        </div>
     );
 }
 
