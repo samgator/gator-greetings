946b5bb2d42ed35eb371ceb7adae5431a118776a
kprhoads <krhoads325@gmail.com>
2025-03-03 19:34:28 -0500

    Added functionality to switch between login and signup pages

--- react-apps/src/Login.jsx
+++ react-apps/src/Login.jsx
@@ -1,19 +1,50 @@
+import { useState } from "react";
+
 function Login() {
-    return(
-        <div className="container">
+
+    const [page, setPage] = useState("login")
+    function loadLogin(){
+        setPage(prevPage => "login")
+    }
+    function loadSignUp(){
+        setPage(prevPage => "signup")
+    }
+
+    if(page == "login"){
+        return(
+            <div className="container">
+                <div className="login">
+                    <img src="https://placehold.co/200"></img> {/* Enter logo here later */}
+                    <h1>Welcome to GatorGreetings</h1>
+                    <h2>Please log in or sign up below</h2>
+                    <div><input type="text" placeholder="Email address"/></div>
+                    <div><input type="password" placeholder="Password"/></div>
+                    <div>
+                        <button className="loginBTN">Log In</button>
+                        <button className="loginBTN" onClick={loadSignUp}>Sign Up</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    else if (page == "signup"){
+        return(
+            <div className="container">
             <div className="login">
                 <img src="https://placehold.co/200"></img> {/* Enter logo here later */}
-                <h1>Welcome to GatorGreetings</h1>
-                <h2>Please log in or sign up below</h2>
+                <h1>Create Your<br/>GatorGreetings Account</h1>
                 <div><input type="text" placeholder="Email address"/></div>
                 <div><input type="password" placeholder="Password"/></div>
+                <div><input type="password" placeholder="Confirm Password"/></div>
                 <div>
-                    <button className="loginBTN">Log In</button>
-                    <button className="loginBTN">Sign Up</button>
+                    <button className="loginBTN">Create Account</button>
+                    <button className="loginBTN" onClick={loadLogin}>Back to Login</button>
                 </div>
             </div>
         </div>
-    );
+        );
+    }
+    
 }
 
 export default Login
--- react-apps/src/main.jsx
+++ react-apps/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Login from './Login.jsx'
-import SignUp from './SignUp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
