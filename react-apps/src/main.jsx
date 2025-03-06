b0023d888f4320fc74d5ea5723e0bfddb87ac5ea
kprhoads <krhoads325@gmail.com>
2025-03-06 16:52:20 -0500

    Added basic profile structure

--- react-apps/src/main.jsx
+++ react-apps/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Login from './Login.jsx'
+import Profile from './Profile.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login/>
+    {/*<Login/>*/}
+    {/*<Profile username="Test User" bio="Test Bio"/>*/}
+    <Profile username="Test User" bio="Test Bio"/>
   </StrictMode>,
 )
