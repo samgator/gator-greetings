ea87220a92298d2546facd89d83066e47ad33e42
hboppana <hboppana@outlook.com>
2025-03-08 13:40:13 -0500

    create user schema using mongoose

---
+++ server/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    googleId: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+  });
+
+
+  module.exports = mongoose.model("User", UserSchema);
+  
+  
