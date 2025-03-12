1de3139d9d66b94fe0de8bb4294dacb2851515ec
AsianRiceX <jtbow1111@gmail.com>
2025-03-12 17:23:06 -0400

    Styled the profile page with border for profile picture, X button, and a softer background

--- react-apps/src/Profile.jsx
+++ react-apps/src/Profile.jsx
@@ -2,12 +2,12 @@ import './Profile.css'
 function Profile({username, bio}) {
     return ( 
         <div className="container">
-            <div className='exit-btn-container'>
-                <button className='exit-btn'>X</button>
-            </div>
             <div className="card" style={{display: "flex", width: "50vw"}}>
                 <div className="column">
                 <img src="https://placehold.co/300" className="profilePic"/>    
+                </div>
+                <div className='exit-btn-container'>
+                        <button className='exit-btn'>X</button>
                 </div>
                 <div className="column">
                     <h1 className="title">{username}'s Profile</h1> 
