0482cb8bb0680c45e0b59ec98409db445ef37fa4
samgator <skaterpro58@icloud.com>
2025-03-12 15:24:59 -0400

    edit connection.js to use mongoose

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,30 +1,18 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const uri = process.env.ATLAS_URI || "";
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-let db;
 
 const connectDB = async () => {
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-    db = client.db("gator-greetings");
-  } catch(err) {
-    console.error("Error Connecting to MongoDB");
+    await mongoose.connect(uri);
+    console.log("Successfully connected to MongoDB using Mongoose!");
+} catch (err) {
+    console.error("Error connecting to MongoDB:", err);
     process.exit(1);
-  }
+}
 };
 
-export { connectDB, db };
+export { connectDB };
