ec0af099cf2e9edfd2cf2cbc06491c901404d25b
samgator <skaterpro58@icloud.com>
2025-04-12 17:09:48 -0400

    Backend routes for notification creation, fetching, deletion

--- server/models/Notification.js
+++ server/models/Notification.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 const notificationSchema = new mongoose.Schema({
     content: { type: String, required: true },
-    recipient: { type: mongoose.Schema.Types.ObjectId, required: true },
-    sender: {type: mongoose.Schema.Types.ObjectId, default: "Deleted User" },
+    recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: "null" },
+    context: { type: mongoose.Schema.Types.ObjectId, required: true }, // The object ID for the message that generated the notification
     createdAt: { type: Date, default: Date.now }
 });
 
